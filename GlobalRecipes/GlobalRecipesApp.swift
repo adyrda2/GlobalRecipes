04375aeea612ca09f8a4abc7c9da637241bf33dd
Angela Dyrda <adyrda2@gmail.com>
2025-01-17 11:07:12 -0600

    Initial commit

---
+++ GlobalRecipes/GlobalRecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlobalRecipesApp.swift
+//  GlobalRecipes
+//
+//  Created by Angela Dyrda on 1/17/25.
+//
+
+import SwiftUI
+
+@main
+struct GlobalRecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GlobalRecipesTests/GlobalRecipesTests.swift
@@ -0,0 +1,17 @@
+//
+//  GlobalRecipesTests.swift
+//  GlobalRecipesTests
+//
+//  Created by Angela Dyrda on 1/17/25.
+//
+
+import Testing
+@testable import GlobalRecipes
+
+struct GlobalRecipesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
